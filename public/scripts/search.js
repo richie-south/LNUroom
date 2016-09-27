b24612e37d095466b8ab4e2137fb2d8149c13d8e
rs222kn <rs222kn@student.lnu.se>
2016-09-27 17:01:55 +0200

    removed vue.js and used vanilla javascript

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,23 +1,47 @@
 'use strict';
 
+function getInputValue(element){
+  return element.value;
+}
+
+function loadNewRoom(searchString){
+  const href = window.location.href;
+  const regex = new RegExp('.*\/');
+  const path = href.match(regex);
+  window.location.href = path[0] + searchString;
+}
+
+function loadRoomIfValid(searchString){
+  if(searchString !== ''){
+    loadNewRoom(searchString);
+  }
+}
+
+function isEnterPress(e){
+  var key = e.which || e.keyCode;
+  if (key === 13) { // enter
+    return true;
+  }
+  return false;
+}
+
 function search(){
-  return new Vue({
-    el: '#searchHolder',
-    data: {
-      searchString: ''
-    },
-    methods: {
-      doSearch: function(){
-        if(this.$data.searchString === ''){
-          return;
-        }
-        const href = window.location.href;
-        const regex = new RegExp('.*\/');
-        const path = href.match(regex);
-        window.location.href = path[0] + this.$data.searchString;
-      }
+  var inputField = document.querySelector('#searchField');
+  var searchButton = document.querySelector('#searchButton');
+
+  inputField.addEventListener('keypress', function(e){
+    if (isEnterPress(e)) {
+      loadRoomIfValid(getInputValue(inputField));
     }
-  });
+  }, false);
+  searchButton.addEventListener('click', function(e){
+    loadRoomIfValid(getInputValue(inputField));
+  }, false);
+  searchButton.addEventListener('keypress', function(e){
+    if (isEnterPress(e)) {
+      loadRoomIfValid(getInputValue(inputField));
+    }
+  }, false);
 }
 
 window.onload = search;
